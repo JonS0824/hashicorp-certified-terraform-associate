dfd9bf939bec3742b1512ec9bf3ff51424087804
Jon Song <jon.song.2020@outlook.com>
2022-05-28 22:41:37 +1200

    Add comments

--- 08-Terraform-Workspaces/v1-local-backend/c4-ec2-instance.tf
+++ 08-Terraform-Workspaces/v1-local-backend/c4-ec2-instance.tf
@@ -3,13 +3,14 @@ resource "aws_instance" "my-ec2-vm" {
   ami           = data.aws_ami.amzlinux.id 
   instance_type = var.instance_type
   key_name      = "terraform-key"
-  count = terraform.workspace == "default" ? 2 : 1    
+  count = terraform.workspace == "default" ? 2 : 1      # if workspace eq default, creating 2 instances, otherwise creating 1 instance for other environment    
 	user_data = file("apache-install.sh")  
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   tags = {
     "Name" = "vm-${terraform.workspace}-${count.index}"
   }
 }
+
 
 
 
