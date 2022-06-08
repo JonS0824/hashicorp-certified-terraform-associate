b475a9f73b2c583870305f958cf50e82f03ed69c
Jon Song <jon.song.2020@outlook.com>
2022-06-08 21:58:39 +1200

    Changes for 02 sessions

--- 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
+++ 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
@@ -11,11 +11,11 @@ terraform {
 # Provider Block
 provider "aws" {
   profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
-  region  = "us-east-1"
+  region  = "ap-southeast-2"
 }
 
 # Resource Block
 resource "aws_instance" "ec2demo" {
-  ami           = "ami-0be2609ba883822ec" # Amazon Linux in us-east-1, update as per your region
+  ami           = "ami-0c6120f461d6b39e9" # Amazon Linux in us-east-1, update as per your region
   instance_type = "t2.micro"
 }
